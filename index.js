e321b93b58e9b87a68e45c84cf1b55ab08801283
taytay836 <javanteshanks@yahoo.com>
2024-08-15 14:15:08 -0500

    done.. all 6

--- index.js
+++ index.js
@@ -85,7 +85,15 @@ let subscriptions = [
   /////////////////////////////////////////////////////////////////
   
   
-  let cancelSubscriptions = function(){
+  let cancelSubscriptions = function(array){
+    //iterate using for loop
+  for (var i = 0; i < array.length; i += 2) {
+    //if cost greater than 10 cancel every other one
+        if (array[i].costPerMonth > 10) {
+          array[i].cancel = true;
+        }
+      }
+    
     
   };
   
@@ -102,8 +110,12 @@ let subscriptions = [
   /////////////////////////////////////////////////////////////////
   
   
-  let subscriptionList = function(){
-   
+  let subscriptionList = function(array){
+      return array.reduce(function(acc, sub) {
+        //get number to correct decimal place then use reduce to fomat answer with only name amd price..
+        return acc + sub.name + ' - ' + sub.costPerMonth.toFixed(2) + '\n';
+      }, '');
+    
   };
   
   
@@ -113,8 +125,25 @@ let subscriptions = [
   // PROBLEM #3 ///////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////
   
-  let getSubscriptionObject = function(){
-   
+  let getSubscriptionObject = function(array, name){
+      // help function 
+      function findSubscription(index) {
+        // base case
+        if (index >= array.length) {
+          return { name: null, costPerMonth: null };
+        }
+        // if the current subscription matches the name
+        if (array[index].name === name) {
+          return { name: array[index].name, costPerMonth: array[index].costPerMonth };
+        }
+        // recursive call 
+        return findSubscription(index + 1);
+      }
+      
+      // search at 0
+      return findSubscription(0);
+    
+    
   };
   
   
@@ -125,7 +154,17 @@ let subscriptions = [
   
   
   
-  let updateSubscription = function(){
+  let updateSubscription = function(object, updates){
+      for (var i = 0; i < updates.length; i++) {
+        //update using index and iterate with for loop
+        var key = updates[i][0];
+        var value = updates[i][1];
+        //if has property object key = the value
+        if (object.hasOwnProperty(key)) {
+          object[key] = value;
+        }
+      }
+      return object;
     
   };
   
@@ -135,8 +174,12 @@ let subscriptions = [
   // PROBLEM #5 ///////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////
   
-  let getMultipleUsers = function(){
-   
+  let getMultipleUsers = function(array){
+    // use native filter method to return object with multiple users
+      return array.filter(function(sub) {
+        return sub.users.length > 1;
+      });
+    
   }; 
   
   
@@ -146,8 +189,14 @@ let subscriptions = [
   // PROBLEM #6 ///////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////
   
-  let getUsersArray = function(){
-   
+  let getUsersArray = function(array){
+      return array.map(function(sub) {
+        //return onlt name property and users prop pointing array of users
+        return {
+          name: sub.name,
+          users: sub.users
+        };
+      });
   };
   
   
